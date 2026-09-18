9dece44bc9acc5b94543726bf2cf50ec3983e1d2
sam-aydev <samadet0001@gmail.com>
2026-09-18 03:47:07 +0100

    feat: configured cors since we are trying to use vercel for deploying this project

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.webhooks import router as webhook_router
 
 app = FastAPI(title="Micro SAAS Orchestrator")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(webhook_router, prefix="/api/v1/webhooks", tags=["Webhooks"])
 
